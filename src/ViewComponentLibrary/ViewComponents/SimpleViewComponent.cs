45b7453f85a44829276fba6a30869746912dcdd2
David Paquette <dave@davepaquette.com>
2016-07-16 07:45:17 -0600

    Initial Commit

---
+++ src/ViewComponentLibrary/ViewComponents/SimpleViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewComponentLibrary.ViewComponents
+{
+    [ViewComponent(Name = "ViewComponentLibrary.Simple")]
+    public class SimpleViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(int number)
+        {
+            return View(number + 1);
+        }
+    }
+}
